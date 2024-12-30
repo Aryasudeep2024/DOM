f2d5dde7947dc4aa349d6bb39c5016b27a5acf17
Aryasudeep2024 <arya32.2011@gmail.com>
2024-12-30 15:45:17 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,13 @@
+let para=document.getElementById("quote")
+para.textContent="Everything Happens For a Reason"
+let changes1 = document.getElementsByClassName("info-box");
+
+for (let i = 0; i < changes1.length; i++) {
+    changes1[i].style.backgroundColor = "red";
+}
+ const listItems=document.getElementsByTagName("li")
+ listItems[2].textContent="ENJOY"
+
+ let contents=document.querySelector("#test")
+
+ contents.textContent="Have a great day"
